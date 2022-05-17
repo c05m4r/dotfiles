b791e92d3ee1edc26e9be3ce6c9c42eee222a2c2
Marcos Gabriel Miller <marcosgabrielmiller@gmail.com>
2022-05-17 04:36:26 -0300

    Add install_fedora.sh

---
+++ install_fedora.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dnf update -y && dnf upgrade -y --refresh
+dnf install -y htop \
+	bat \
+	lsd \
+        mc \
+        neofetch \
+        git \
+        ncdu \
+        neovim \
+        powerline \
+        fzf \
+        kitty \
+        feh \
+        rofi \
+	polybar \
+	keepassxc \
+	redshift \
+        conky \
+        firefox \
+#flatpak
+flatpak install flathub com.brave.Browser
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#configure
+dnf clean all
+dnf autoremove -y
+#oh-my-bash, nerd-fonts, megasync, google-chrome, vlc
