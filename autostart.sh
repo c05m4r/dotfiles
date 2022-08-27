8464d1d1c7c31291226dd722e354f5a7c9cadeb7
Marcos Gabriel Miller <marcosgabrielmiller@gmail.com>
2022-08-27 12:46:20 -0300

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,5 +2,5 @@
 redshift -O 3000 &
 feh --bg-scale ~/dotfiles/wallpapers/wallpaper4.jpg &
 sleep 10
-diodon &
-conky -c ~/dotfiles/.config/conky/conky.conf &
+#diodon &
+#conky -c ~/dotfiles/.config/conky/conky.conf &
